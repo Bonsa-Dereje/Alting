aa0f2e227ffa5ba93ad9d3ec83cfc9b6b8232a7e
Yoseph F. Lemma <yosephfetene00@gmail.com>
2026-07-18 11:26:27 +0300

    DB CONFIG

---
+++ src/main/java/org/alting/Main.java
@@ -0,0 +1,31 @@
+package org.alting;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Main {
+    public static void main(String [] args) {
+        Dotenv dotenv = Dotenv.load();
+        String host = dotenv.get("DB_HOST");
+        String port = dotenv.get("DB_PORT");
+        String database = dotenv.get("DB_DATABASE");
+        String username = dotenv.get("DB_USERNAME");
+        String password = dotenv.get("DB_PASSWORD");
+
+        String url = String.format(
+            "jdbc:postgresql://%s:%s/%s",
+            host,
+            port,
+            database
+        );
+
+        try (
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to the database successfully!");
+            connection.close();
+        ) catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
